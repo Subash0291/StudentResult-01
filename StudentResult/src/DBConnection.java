7df063abd14971fc8d1f4d4b78365a05d7acffdb
Subash0291 <kingsubash0007@gmail.com>
2026-04-23 16:15:04 +0530

    Update DBConnection.java

--- StudentResult/src/DBConnection.java
+++ StudentResult/src/DBConnection.java
@@ -3,16 +3,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/student_result";
-    private static final String USER = "root";
-    private static final String PASSWORD = "awdr1357908642@A";
-
     public static Connection getConnection() throws SQLException {
+        String host = System.getenv("MYSQLHOST") != null ? System.getenv("MYSQLHOST") : "localhost";
+        String port = System.getenv("MYSQLPORT") != null ? System.getenv("MYSQLPORT") : "3306";
+        String db   = System.getenv("MYSQLDATABASE") != null ? System.getenv("MYSQLDATABASE") : "student_result";
+        String user = System.getenv("MYSQLUSER") != null ? System.getenv("MYSQLUSER") : "root";
+        String pass = System.getenv("MYSQLPASSWORD") != null ? System.getenv("MYSQLPASSWORD") : "awdr1357908642@A";
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + db + "?useSSL=false&allowPublicKeyRetrieval=true";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, pass);
     }
 }
